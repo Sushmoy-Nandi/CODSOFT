3136be1e23615afba978ccf9ded6b698b1d38a65
“Sushmoy <learnsushmoysk1091@gmail.com>
2024-06-09 22:30:17 +0600

    Creating Second Signup class and update first Signup class

--- Task 3/src/in/codsoft/ATM/Signup.java
+++ Task 3/src/in/codsoft/ATM/Signup.java
@@ -225,7 +225,7 @@ public class Signup extends JFrame implements ActionListener {
                Connn con1 = new Connn();
                String q = "insert into signup values('"+formno+"', '"+name+"','"+fname+"','"+dob+"','"+gender+"','"+email+"','"+marital+"', '"+address+"', '"+city+"','"+pincode+"','"+state+"' )";
                con1.statement.executeUpdate(q);
-               new Signup2();
+               new Signup2(first);
                setVisible(false);
            }
 
