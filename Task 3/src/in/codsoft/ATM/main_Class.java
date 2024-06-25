33dd208ac9a9b31b1f26b0e8a95263898ddac4a6
“Sushmoy <learnsushmoysk1091@gmail.com>
2024-06-25 13:00:26 +0600

    Creating Pin Change Class

--- Task 3/src/in/codsoft/ATM/main_Class.java
+++ Task 3/src/in/codsoft/ATM/main_Class.java
@@ -100,6 +100,10 @@ public class main_Class extends JFrame implements ActionListener {
         }else if(e.getSource()==b3){
             new FastCash(pin);
             setVisible(false);
+        } else if (e.getSource()==b5) {
+            new Pin(pin);
+            setVisible(false);
+
         }
 
     }
