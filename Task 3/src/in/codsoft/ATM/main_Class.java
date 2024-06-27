12dc390ed59bdaf2a01002c0b79c18c614f980a2
“Sushmoy <learnsushmoysk1091@gmail.com>
2024-06-27 11:33:04 +0600

    Creating Mini Statement class and update main class

--- Task 3/src/in/codsoft/ATM/main_Class.java
+++ Task 3/src/in/codsoft/ATM/main_Class.java
@@ -103,7 +103,8 @@ public class main_Class extends JFrame implements ActionListener {
         } else if (e.getSource()==b5) {
             new Pin(pin);
             setVisible(false);
-
+        }else if(e.getSource()==b4){
+            new mini_statement(pin);
         }
 
     }
